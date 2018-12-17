3691eb904b26caec79015c6f2e81291cd84c7f6d
Christian Alexander Wolf <mail@cawolf.de>
2018-12-17 14:11:59 +0100

    added monolog

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Voryx\ThruwayBundle\VoryxThruwayBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
