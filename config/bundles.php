14620205c68bc38acb0fe2bb915296c2aaf69210
Martin Tomala <cheesaw@gmx.net>
2018-12-17 18:12:24 +0100

    add maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Voryx\ThruwayBundle\VoryxThruwayBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
