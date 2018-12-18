8ab07aecb8f4a67cab496e1d30ffb589523e7bc0
Christian Alexander Wolf <mail@cawolf.de>
2018-12-18 12:15:28 +0100

    started authentication for websocket

---
+++ src/Security/InMemoryProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\InMemoryUserProvider;
+
+class InMemoryProvider extends InMemoryUserProvider
+{
+    public function __construct(string $userPassword)
+    {
+        parent::__construct([
+            'phash-board' => [
+                'password' => $userPassword,
+                'roles' => [
+                    'ROLE_USER'
+                ]
+            ]
+        ]);
+    }
+}
